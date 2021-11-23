316c1c5d21d15501d4904675e3dcefba36604e52
gloxinia106 <74160184+gloxinia106@users.noreply.github.com>
2021-11-23 21:01:47 +0900

    확률 계산 코드(미완성)

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,8 @@
-const listForm = document.getElementById("form");
+// import { Chart } from "chart.js";
+// import ChartDataLabels from "chartjs-plugin-datalabels";
+
+const PercentInput = document.getElementById("percent-0");
+const NameInput = document.getElementById("name-0");
 
 let listId = [];
 let NameData = [];
@@ -12,62 +16,100 @@ const deleteToList = (e) => {
 };
 
 const addToList = () => {
+  const ul = document.getElementById("form");
   const id = listId[listId.length - 1] + 1;
 
-  const div = document.createElement("div");
-  div.id = `list-${id}`;
-  div.className = "list";
+  const li = document.createElement("li");
+  li.id = `list-${id}`;
+  li.className = "list";
 
   const nameInput = document.createElement("input");
   nameInput.id = `name-${id}`;
   nameInput.className = "input-name";
+  nameInput.addEventListener("keypress", handleNameInput);
 
   const percentInput = document.createElement("input");
   percentInput.id = `percent-${id}`;
   percentInput.className = "input-percent";
+  percentInput.type = "number";
+  percentInput.step = "0.01";
+  percentInput.min = "0";
+  percentInput.max = "100";
+  percentInput.value = PercentData[PercentData.length - 1];
+  percentInput.addEventListener("keypress", handlePercentInput);
 
   const button = document.createElement("button");
   button.innerText = "delete";
   button.addEventListener("click", deleteToList);
 
-  listForm.appendChild(div);
-  div.appendChild(nameInput);
-  div.appendChild(percentInput);
-  div.appendChild(button);
+  ul.appendChild(li);
+  li.appendChild(nameInput);
+  li.appendChild(percentInput);
+  li.appendChild(button);
 };
 
-const handleSubmit = (e) => {
-  e.preventDefault();
-  const listDiv = [].slice.call(e.target.children);
-  let array = [];
-  let nameArray = [];
-  let percentArray = [];
-  listDiv.forEach((div) => {
-    if (div.tagName == "DIV") {
-      const id = div.id.split("-")[1];
-      array.push(parseInt(id));
-      const inputName = div.children[0].value;
-      const inputPercent = div.children[1].value;
-      nameArray.push(inputName);
-      percentArray.push(parseFloat(inputPercent));
-    }
+const calculatePercent = (value) => {
+  let sum = 0;
+  PercentData.map((data) => {
+    sum += data;
   });
-  listId = array;
-  NameData = nameArray;
-  PercentData = percentArray;
-  addToList();
+  if (sum !== 0) {
+    let percentage = ((value * 100) / sum).toFixed(2);
+    return parseFloat(percentage);
+  } else {
+    return 100;
+  }
 };
 
-listForm.addEventListener("submit", handleSubmit);
+const handleEnterKey = () => {
+  let tempids = [];
+  const PercentInputs = document.querySelectorAll(".input-percent");
+  const NameInputs = document.querySelectorAll(".input-name");
+  PercentData = [...PercentInputs].map((input) => {
+    const id = input.id.split("-")[1];
+    tempids.push(parseInt(id));
+    const calVal = calculatePercent(input.value);
+    input.value = calVal;
+    return calVal;
+  });
+  NameData = [...NameInputs].map((input) => input.value);
+  listId = tempids;
+  addToList();
+  console.log(PercentData);
+};
 
-// import { Chart } from "chart.js";
+const handlePercentInput = (e) => {
+  if (e.key === "Enter") {
+    handleEnterKey();
+  } else {
+  }
+  // const value = parseInt(e.target.value);
+  // const index = e.target.id.split("-")[1];
+  // PercentData.splice(index, 1, value);
+  // UpdateToChart();
+};
+
+const handleNameInput = (e) => {
+  if (e.key === "Enter") {
+    handleEnterKey();
+  } else {
+  }
+};
+
+PercentInput.addEventListener("keypress", handlePercentInput);
+NameInput.addEventListener("keypress", handleNameInput);
+
+// const UpdateToChart = () => {
+//   myChart.data.datasets[0].data = PercentData;
+//   myChart.update();
+// };
 
 // const ctx = document.getElementById("pieChart").getContext("2d");
 
 // const data = {
 //   datasets: [
 //     {
-//       data: [10, 10, 10, 10, 10],
+//       data: [10, 20, 30, 20, 20],
 //       backgroundColor: [
 //         "rgba(255, 99, 132, 1)",
 //         "rgba(54, 162, 235, 1)",
@@ -85,13 +127,53 @@ listForm.addEventListener("submit", handleSubmit);
 //   options: {
 //     responsive: false,
 //     events: [],
-//     elements: {
-//       arc: {
-//         borderColor: "#000000",
+//     plugins: {
+//       datalabels: {
+//         formatter: (value, ctx) => {
+//           let sum = 0;
+//           let dataArr = ctx.chart.data.datasets[0].data;
+//           dataArr.map((data) => {
+//             sum += data;
+//           });
+//           let percentage = ((value * 100) / sum).toFixed(2) + "%";
+//           return percentage;
+//         },
 //       },
 //     },
+//     // elements: {
+//     //   arc: {
+//     //     borderColor: "#000000",
+//     //   },
+//     // },
 //   },
+//   plugins: [ChartDataLabels],
 // };
 // const myChart = new Chart(ctx, config);
 
-// console.log(myChart.data.datasets);
+// console.log(myChart.options.elements.arc);
+
+// const handleSubmit = (e) => {
+//   e.preventDefault();
+//   let tempids = [];
+//   NameData = [...nameInputs].map((input) => {
+//     const id = input.id.split("-")[1];
+//     tempids.push(parseInt(id));
+//     return input.value;
+//   });
+//   PercentData = [...percentInputs].map((input) => {
+//     if (input.value === "100" || input.value === "") {
+//       const percentValue = Math.round((100 / percentInputs.length) * 100) / 100;
+//       return percentValue;
+//     }
+//     return input.value;
+//   });
+//   console.log(PercentData);
+//   listId = tempids;
+//   addToList();
+//   // UpdateToChart();
+//   percentInputs = document.querySelectorAll(".input-percent");
+//   nameInputs = document.querySelectorAll(".input-name");
+//   percentInputs.forEach((input) =>
+//     input.addEventListener("keypress", handlePercentInput)
+//   );
+// };
